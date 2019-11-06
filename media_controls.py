a640063c3acd1d9de11b377829314aa6a747473e
mrhaack <mrhaack@yandex.ru>
2019-11-07 00:33:49 +0300

    Returned a variable :)

--- media_controls.py
+++ media_controls.py
@@ -1,7 +1,8 @@
 from pynput.keyboard import KeyCode, Controller
 
 keyboard = Controller()
+VK_MEDIA_PLAY_PAUSE = 0x1008FF14
 
 def toggle_play():
-    keyboard.press(KeyCode.from_vk(0x1008FF14))
-    keyboard.release(KeyCode.from_vk(0x1008FF14))
+    keyboard.press(KeyCode.from_vk(VK_MEDIA_PLAY_PAUSE))
+    keyboard.release(KeyCode.from_vk(VK_MEDIA_PLAY_PAUSE))
